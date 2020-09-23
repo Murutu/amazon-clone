2bfe70a132e9032d6677f4be87b573d0777e2605
Murutu <murutupeter@email.com>
2020-09-23 13:42:16 +0300

    added backend link

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/challenge-82441/us-central1/api'
+    baseURL: 'https://us-central1-challenge-82441.cloudfunctions.net/api'
+    // 'http://localhost:5001/challenge-82441/us-central1/api'
 });
 
 export default instance;
+
